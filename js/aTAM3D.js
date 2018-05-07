945bcafe55551f5ba3a58605e908bfaad6115247
Anshul <anshulr28@gmail.com>
2018-05-07 01:46:20 -0700

    reading config files

--- js/aTAM3D.js
+++ js/aTAM3D.js
@@ -4,6 +4,8 @@ function aTAM3D( ) {
   this.history = [];
   this.temperature = 2;
   this.currIdx = -1;
+  this.seedAssembly = new Assembly();
+  this.currAssembly = new Assembly();
 
   this.load = function(path) {
     var request = new XMLHttpRequest();
@@ -15,15 +17,35 @@ function aTAM3D( ) {
           for (var i in allText) {
             if (allText[i][0] === 'T') {
               // Add tile
-              //alert('T');
+              var tileInfo = allText[i].split(' ');
+              var tile = new TileType(tileInfo[1], tileInfo[3:], tileInfo[2]);
+              this.tileset[tileInfo[1]] = tile;
             }
             else if (allText[i][0] === 'B') {
               // Add bond
-              //alert('B');
+              var bondInfo = allText[i].split(' ');
+              if (!(bondInfo[1] in this.bondStrengths)) {
+                this.bondStrengths[bondInfo[1]] = {};
+              }
+              this.bondStrengths[bondInfo[1]][bondInfo[2]] = Number(bondInfo[3]);
+              if (!(bondInfo[2] in this.bondStrengths)) {
+                this.bondStrengths[bondInfo[2]] = {};
+              }
+              this.bondStrengths[bondInfo[2]][bondInfo[1]] = Number(bondInfo[3]);
             }
             else if (allText[i][0] === 'S') {
-              // Add seed
-              //alert('S');
+              // Add seed tile
+              var seedTileInfo = allText[i].split(' ');
+              var tiletype = this.tileset[seedTileInfo[1]];
+              var position = [Number(seedTileInfo[2]), Number(seedTileInfo[3]), Number(seedTileInfo[4])];
+              var tile = new Tile(
+                tiletype,
+                position,
+                Number(seedTileInfo[5]),
+                Number(seedTileInfo[6])
+              );
+              this.seedAssembly.addTile(tile);
+              this.currAssembly.addTile(tile);
             }
           }
         }
@@ -39,12 +61,24 @@ function aTAM3D( ) {
 }
 
 function Assembly() {
-  this.tile_positions = [];
+  this.tiles = [];
+  this.addTile = function(tile) {
+    this.tiles.push(tile);
+  }
 }
 
-function Tile(name, bonds, color) {
+function TileType(name, bonds, color) {
   this.name = name;
-  this.color = color;
+
+  if (color === 'R') {
+    this.color = 0xff0000;
+  } else if (color === 'G') {
+    this.color = 0x00ff00;
+  } else if (color === 'B') {
+    this.color = 0x0000ff;
+  } else {
+    this.color = 0xffffff;
+  }
 
   this.north = bonds[0];
   this.east = bonds[1];
@@ -52,6 +86,13 @@ function Tile(name, bonds, color) {
   this.west = bonds[3];
   this.up = bonds[4];
   this.down = bonds[5];
+}
+
+function Tile(tiletype, position, upSide, northSide) {
+  this.tiletype = tiletype
+  this.position = position
+  this.upSide = upSide
+  this.northSide = northSide
 }
 // Assembly = function () {
 //   tiles: [],
